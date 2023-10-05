cd4ad9e2e64e9dcade68a17c20f043143c390577
Anais Moutarlier <anaismoutarlier@gmail.com>
2023-10-05 19:43:36 +0200

    add cronjob and server functionality

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const scrap = require("./scraper");
+const express = require("express");
+const http = require("http");
+const { CronJob } = require("cron");
 
-scrap();
+const app = express();
+const server = http.createServer(app);
+
+function cron() {
+  const job = new CronJob(
+    "* 31 19 * * *",
+    () => scrap(),
+    null,
+    true,
+    "Europe/Paris"
+  );
+  job.start();
+}
+
+cron();
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- scraper.js
+++ scraper.js
@@ -6,12 +6,12 @@ const scrap = async () => {
   try {
     var browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on("request", request => {
-      if (["stylesheet", "image", "font"].includes(request.resourceType()))
-        request.abort();
-      else request.continue();
-    });
+    // await page.setRequestInterception(true);
+    // page.on("request", request => {
+    //   if (["stylesheet", "image", "font"].includes(request.resourceType()))
+    //     request.abort();
+    //   else request.continue();
+    // });
 
     await page.goto(
       "https://www.welcometothejungle.com/fr/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&refinementList%5Bcontract_type%5D%5B%5D=FULL_TIME&refinementList%5Bcontract_type%5D%5B%5D=INTERNSHIP&refinementList%5Bcontract_type%5D%5B%5D=TEMPORARY&query=javascript%20developer&page=1&sortBy=mostRecent",
@@ -20,15 +20,15 @@ const scrap = async () => {
       }
     );
 
-    const totalJobs = await page.$eval(
-      "div[data-testid='jobs-search-results-count']",
-      el => {
-        if (el) {
-          return Number(el.textContent.trim());
-        }
-        return null;
-      }
-    );
+    // const totalJobs = await page.$eval(
+    //   "div[data-testid='jobs-search-results-count']",
+    //   el => {
+    //     if (el) {
+    //       return Number(el.textContent.trim());
+    //     }
+    //     return null;
+    //   }
+    // );
 
     const limitDate = moment().subtract(1, "days").toDate();
     console.log(limitDate);
@@ -38,7 +38,7 @@ const scrap = async () => {
       (arr, limitDate) => {
         return arr
           .map(el => {
-            const url = el.querySelector("a").href;
+            const url = el.querySelector("a")?.href;
             const title = el.querySelector("h4").textContent.trim();
             const tags = Array.from(
               el.querySelectorAll("div[role='listitem']")
@@ -51,16 +51,45 @@ const scrap = async () => {
       },
       limitDate
     );
+    console.log("Found job list", jobList.length);
+    for (const job of jobList) {
+      await page.goto(job.url, { waitUntil: "networkidle0" });
+
+      const data = await page.evaluate(() => {
+        const data = {};
+        const company = document.querySelector("div.sc-bXCLTC.dPVkkc");
+        const companyUrl = company.querySelector("a").href;
+        const companyName = company.textContent.trim();
+        data.company = { name: companyName, url: companyUrl };
+        data.sections = [
+          ...document.querySelectorAll("section[data-testid*='job-section-']"),
+        ].map(el => {
+          const title = el.querySelector("h2")?.textContent;
+          const content = [...el.querySelectorAll("p")]
+            .map(p => p.textContent)
+            .join("");
+          return { title, content };
+        });
+        return data;
+      });
+      job.data = data;
+      //   break;
+    }
+    // const [res1, res2] = await Promise.allSettled([
+    //   page.waitForNavigation(),
+    //   await page.click("li.ais-Hits-list-item a"),
+    // ]);
+    // console.log(res1, res2);
     await fs.writeFile(
       `./data/jobs-${Date.now()}.json`,
       JSON.stringify(jobList, null, 2)
     );
-    console.log(totalJobs);
-    console.log(jobList.length);
+    // console.log(totalJobs);
+    // console.log(jobList.length);
   } catch (error) {
     console.error(error);
   } finally {
-    // await browser.close();
+    await browser.close();
   }
 };
 
